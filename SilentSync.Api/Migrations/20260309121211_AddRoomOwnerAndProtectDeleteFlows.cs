2bce6f549b34564d5cd0ca1c91d40945190a6a2f
Anderson Krause <161200241+AndersonKrause@users.noreply.github.com>
2026-03-09 13:28:11 +0100

    add authorize by endpoints, change the criterian to delete user and room. (#10)

---
+++ SilentSync.Api/Migrations/20260309121211_AddRoomOwnerAndProtectDeleteFlows.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SilentSync.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomOwnerAndProtectDeleteFlows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Rooms",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_OwnerId",
+                table: "Rooms",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Users_OwnerId",
+                table: "Rooms",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Users_OwnerId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_OwnerId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Rooms");
+        }
+    }
+}
