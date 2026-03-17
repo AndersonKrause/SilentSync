891b99f0f20e3321e8b7efe41c90686809085f08
Anderson Krause <161200241+AndersonKrause@users.noreply.github.com>
2026-03-17 16:22:20 +0100

    test(auth): add unit test for DeleteUserByEmailAsync (#31)
    - setup test project with xUnit
    - added InMemory database factory
    - created FakeLoginCodeService
    - implemented first AuthService test

---
+++ SilentSync.Tests/Helpers/FakeLogingCodeService.cs
@@ -0,0 +1,16 @@
+﻿using SilentSync.Api.Services.Auth;
+
+namespace SilentSync.Tests.Helpers;
+
+public class FakeLoginCodeService : ILoginCodeService
+{
+    public Task SendLoginCodeAsync(string email)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ConsumeValidCodeAsync(string email, string code)
+    {
+        return Task.CompletedTask;
+    }
+}
