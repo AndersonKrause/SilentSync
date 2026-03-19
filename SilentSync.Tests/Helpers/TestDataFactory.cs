10aa0cbbcd9b8869d63e1d8062acc53bbf26499e
Anderson Krause <161200241+AndersonKrause@users.noreply.github.com>
2026-03-19 11:21:51 +0100

    test: use test data factory in room service tests

---
+++ SilentSync.Tests/Helpers/TestDataFactory.cs
@@ -0,0 +1,26 @@
+﻿using SilentSync.Api.Models;
+
+namespace SilentSync.Tests.Helpers;
+
+public static class TestDataFactory
+{
+    public static AppUser CreateUser(Guid? id = null, string email = "user@test.com", string role = "user")
+    {
+        return new AppUser
+        {
+            Id = id ?? Guid.NewGuid(),
+            Email = email,
+            Role = role
+        };
+    }
+
+    public static Room CreateRoom(Guid ownerId, string code = "ABC123", Guid? id = null)
+    {
+        return new Room
+        {
+            Id = id ?? Guid.NewGuid(),
+            Code = code,
+            OwnerId = ownerId
+        };
+    }
+}
