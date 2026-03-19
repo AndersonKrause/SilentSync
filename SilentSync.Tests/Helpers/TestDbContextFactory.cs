52cf0d688e62bb65868bbbcf89a0006874fb10c6
Anderson Krause <161200241+AndersonKrause@users.noreply.github.com>
2026-03-19 10:56:40 +0100

    test: replace InMemory provider with SQLite in TestDbContextFactory test: update tests to support SQLite in-memory connection lifecycle test: fix failing tests due to relational constraints in SQLite

--- SilentSync.Tests/Helpers/TestDbContextFactory.cs
+++ SilentSync.Tests/Helpers/TestDbContextFactory.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using SilentSync.Api.Data;
 
 namespace SilentSync.Tests.Helpers;
 
 public static class TestDbContextFactory
 {
-    public static AppDbContext Create()
+    public static (AppDbContext Context, SqliteConnection Connection) Create()
     {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+
         var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .UseSqlite(connection)
             .Options;
 
-        return new AppDbContext(options);
+        var context = new AppDbContext(options);
+        context.Database.EnsureCreated();
+
+        return (context, connection);
     }
 }
